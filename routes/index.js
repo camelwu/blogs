478a52516ea30a46829b5a7d2621cab05e7aa154
camelwu <camelwu963@126.com>
2019-07-30 16:12:18 +0800

    Update index.js
    Update index.js split front-page and manager two parts

--- routes/index.js
+++ routes/index.js
@@ -1,144 +1,20 @@
-const fs = require('fs'),
-  url = require('url'),
-  setting = require('../settings'),
-  tit = {
-  "/": "首页",
-  "login": "登录",
-  "signup": "注册",
-  "admin": "管理",
-  "news": "媒体动态",
-  "Recruitment": "诚聘英才",
-  "service": "业务范围",
-  "aboutus": "关于我们",
-  "contactus": "联系我们",
-}
-
-// middleware
-function checkLogin(req, res, next) {
-  if (!req.session.user) {
-    res.redirect('/')
-  }
-  next()
-}
-function checkNotLogin(req, res, next) {
-  if (req.session.user) {
-    res.redirect('back')
-  }
-  next()
-}
-// get访问
-function front_get(req, res) {
-  if ('/' == req.path) {
-    res.render('index', { title: tit['/'], db: {} })
-  } else {
-    let uriAry = req.path.split('/'),
-      path = uriAry[1],
-      exists = fs.existsSync('./views/' + path + '.html');
-    if (exists) {
-      res.render(path, { title: tit[path], db: 'Something blew up!' })
-    } else {
-      res.redirect('/404')
-    }
-  }
-}
-function handle_get(req, res) {
-  let path, uriAry = req.path.split('/'),
-    uri = uriAry[1],
-    exists = fs.existsSync('./views/' + uri + '.html');
-  if (uriAry.length < 3) {
-    path = 'admin/index'
-    exists = fs.existsSync('./views/' + path + '.html')
-  } else {
-    path = uriAry[2] == '' ? 'admin/index' : 'admin/' + uriAry[2]
-    exists = fs.existsSync('./views/' + path + '.html')
-  }
-  console.log(path)
-
-  if (exists) {
-    let status, result
-    switch (path) {
-      case 'article':
-
-        break;
-      case 'company':
-
-        break;
-      case 'reg':
-
-        break;
-      default:
-        break;
-    }
-    res.render(path, { title: tit[path], setting: setting, error: 'Something blew up!' })
-  } else {
-    res.status(404).send({ error: '404!' });
-  }
-}
-// post请求
-function handle_post(req, res) {
-  const key = req.path,
-    mime = "application/json;charset=utf-8",
-    body = req.body
-  let status, result
-  switch (key) {
-    case 'article':
-
-      break;
-    case 'company':
-
-      break;
-    case 'reg':
-
-      break;
-    default:
-      break;
-  }
-  res.writeHead(200, { "Content-Type": mime });
-  res.status(200).send({ error: '404!' });
-}
-function user_post(req, res){
-  const key = req.path,
-    mime = "application/json;charset=utf-8",
-    body = req.body
-  let status, msg
-  switch (key) {
-    case 'login':
-      msg = {
-        email: body.email,
-        password: body.password
-      }
-      break;
-    case 'signup':
-
-      break;
-    case 'reg':
-
-      break;
-    default:
-      break;
-  }
-  //res.writeHead(200, { "Content-Type": mime });
-  res.status(200).send({ msg: msg });
-}
-// delete请求
-function handle_delete(req, res) {
-  res.render('index', { title: 'Express', cover: {} })
-}
-// put请求
-function handle_put(req, res) {
-  res.render('login', { title: 'Express', cover: {} })
-}
+const settings = require('../settings'),
+    page = require('./front-page'),
+    manager = require('./manager');
 
 module.exports = function (app) {
-  // 正则，前台和管理区分
-  app.get('/' + setting.manage + '(/*)?', [handle_get])
-  app.get('/*', front_get)
-  // 
-  app.post('/login', user_post)
-  app.post('/signup', user_post)
-  app.post('*', [checkLogin, handle_post])
-  // 
-  app.put('*', [checkLogin, handle_put])
-  // 
-  app.delete('*', [checkLogin, handle_delete])
+    // manager center
+    app.get('/' + settings.manage + '(/*)?', [manager.checkLogin, manager.handle_get])
+    app.get('/logout', function (req, res) {
+        req.session.user = null
+        res.redirect('/login?' + Date.now())
+    })
+    // post & delete operation
+    app.post('/article', manager.cpUpload, manager.handle_post)
+    app.post('*', manager.handle_post)
+    app.delete('*', [manager.checkLogin, manager.handle_delete])
+    // font web
+    app.get('/', page.home)
+    app.get('/:slug', page.channel)
+    app.get('/news/:str', page.news)
 };
